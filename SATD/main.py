49ee65edab38fa4912b85ec1729385bfd0600d41
Viny-Pereira <vinypereira75@gmail.com>
2024-12-03 22:17:41 -0300

    ajuste funcao principal

--- SATD/main.py
+++ SATD/main.py
@@ -1,7 +1,26 @@
 import tkinter as tk
 from Interface import DesignApp
 
+class Main:
+    """
+    Main class to initialize and run the DesignApp interface.
+    """
+
+    def __init__(self):
+        """
+        Initializes the Tkinter root and the DesignApp application.
+        """
+        self.root = tk.Tk()
+        self.app = DesignApp(self.root)
+
+    def run(self):
+        """
+        Runs the main loop of the Tkinter application.
+        """
+        self.root.mainloop()
+
+
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = DesignApp(root)
-    root.mainloop()
+    # Create an instance of Main and run the application
+    main_app = Main()
+    main_app.run()
